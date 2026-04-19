603f62631f3d6194b9f33d06a32d4941b237b2fe
anass <anasettai4@gmail.com>
2026-04-20 00:05:39 +0100

    fix icons

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ __('Login') }} - OFPPT eDT Pro</title>
+    <title>{{ __('Login') }} - OFPPT - Emploi du Temps</title>
+    <link rel="icon" href="{{ asset('favicon.ico') }}">
     <script src="https://cdn.tailwindcss.com"></script>
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap');
@@ -375,7 +376,7 @@
                 <div class="logo-box">
                     <img
                         src="{{ asset('images/ofppt_login.png') }}"
-                        alt="OFPPT"
+                        alt="OFPPT Logo"
                         style="
                             width:64px;
                             height:64px;
